22e892e79d82b63e44e44a8385856208891201e1
Alex Davis <8m3xlt@gmail.com>
2024-12-01 13:59:44 +0000

    code golf

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -5,3 +5,8 @@ right = sorted([int(line[1]) for line in lines])
 left = [i*right.count(i) for i in left]
 # distances = [abs(left[i] - right[i]) for i in range(len(left))]
 print(sum(left))
+
+
+# Code golf
+
+print(sum([i*sorted([int(line[1]) for line in [line.split() for line in open("input.txt", "r").read().splitlines()]]).count(i) for i in sorted([int(line[0]) for line in [line.split() for line in open("input.txt", "r").read().splitlines()]])]))
