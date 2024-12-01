a58188d0aed44401e8d206bcfdca968be8864841
Alex Davis <8m3xlt@gmail.com>
2024-12-01 13:56:40 +0000

    1b

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,6 +1,7 @@
 with open("input.txt", "r") as file:
     lines = [line.split() for line in file.read().splitlines()]
-left = sorted([line[0] for line in lines])
-right = sorted([line[1] for line in lines])
-distances = [abs(int(left[i]) - int(right[i])) for i in range(len(left))]
-print(sum(distances))
+left = sorted([int(line[0]) for line in lines])
+right = sorted([int(line[1]) for line in lines])
+left = [i*right.count(i) for i in left]
+# distances = [abs(left[i] - right[i]) for i in range(len(left))]
+print(sum(left))
