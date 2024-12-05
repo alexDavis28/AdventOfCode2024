5e460af6a48dd11e6fb79609d5702dbc38447de3
Alex Davis <8m3xlt@gmail.com>
2024-12-05 10:58:37 +0000

    3b

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -9,3 +9,17 @@ num_match = r"\d+"
 mul_pairs = [[int(i) for i in re.findall(num_match, mul)] for mul in muls]
 mul_products = [a*b for [a,b] in mul_pairs]
 print(sum(mul_products))
+
+# Still want to do the second part in a regex but this works
+print(text)
+text_mod = "do()" + text
+print(text_mod)
+
+dosplit = text_mod.split("do()")[1:]
+activetext = "".join([i.split("don't()")[0] for i in dosplit])
+
+muls = re.findall(mul_match, activetext)
+num_match = r"\d+"
+mul_pairs = [[int(i) for i in re.findall(num_match, mul)] for mul in muls]
+mul_products = [a*b for [a,b] in mul_pairs]
+print(sum(mul_products))
